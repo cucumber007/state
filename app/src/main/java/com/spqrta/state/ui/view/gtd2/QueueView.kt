78bd027d9cc981a567fbbe5ce6f063141f72f735
Pavel Kuznetsov <spqrta@gmail.com>
2024-08-05 15:27:13 +0300

    Inactive queue

--- app/src/main/java/com/spqrta/state/ui/view/gtd2/QueueView.kt
+++ app/src/main/java/com/spqrta/state/ui/view/gtd2/QueueView.kt
@@ -6,19 +6,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import com.spqrta.state.common.logic.features.gtd2.element.Queue
 
 @Composable
 fun QueueView(queue: Queue) {
     Column {
-        Text(text = queue.name)
-        Box(modifier = Modifier.padding(start = 16.dp)) {
-            Column {
-                for (task in queue.elements) {
-                    ElementView(task)
+        if (queue.active) {
+            Text(text = queue.name)
+            Box(modifier = Modifier.padding(start = 16.dp)) {
+                Column {
+                    for (task in queue.elements) {
+                        ElementView(task)
+                    }
                 }
             }
+        } else {
+            Text(text = queue.name, style = TextStyle(color = Color.Gray))
         }
     }
 }
